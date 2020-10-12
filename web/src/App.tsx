aadb18e2336d22e8d1bf2fe676f13b4d94401f77
Sergio Ricardo <sergio-ricardo@hotmail.com>
2020-10-12 18:39:45 -0300

    Aula 1: landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi'
 
 import './styles/global.css';
 import './styles/pages/landing.css';
@@ -10,6 +11,20 @@ function App() {
     <div id="page-landing">
       <div className="content-wrapper">
         <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+        </main>
+
+        <div className="location">
+          <strong>Angra dos Reis</strong>
+          <span>Rio de Janeiro</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </a>
       </div>
     </div>
   );
