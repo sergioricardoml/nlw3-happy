7b300353ce26175fbc5289ea138163684363772e
Sergio Ricardo <sergio-ricardo@hotmail.com>
2020-10-12 19:39:35 -0300

    Aula 1: acrescentar mapas

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import mapMarketImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap() {
   return (
-    <h1>Hellow World</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarketImg} alt=""/>
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita!</p>
+        </header>
+
+        <footer>
+          <strong>Angra dos Reis</strong>
+          <span>Rio de Janeiro</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-23.006165,-44.2952528]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+      
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+      </Map>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+    </div>
   );
 }
 
