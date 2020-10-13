a4cc60ae9c3df27c1448ac2a98c9c02c0175fe40
Sergio Ricardo <sergio-ricardo@hotmail.com>
2020-10-13 18:25:45 -0300

    Aula 2: Config BD e criar tabelas

---
+++ backend/src/server.ts
@@ -0,0 +1,39 @@
+import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
+
+import './database/connection';
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/orphanages', async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  });
+
+  await orphanagesRepository.save(orphanage);
+
+  return response.status(201).json(orphanage);
+});
+
+app.listen(3333);
