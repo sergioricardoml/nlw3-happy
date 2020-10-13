3493681320e91587efedcba1fb06950a61b047d1
Sergio Ricardo <sergio-ricardo@hotmail.com>
2020-10-13 19:31:33 -0300

    Aula 2: Config multer e post com imagens

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -6,7 +6,9 @@ export default {
   async index(request: Request, response: Response) {
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanages = await orphanagesRepository.find();
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images']
+    });
 
     return response.json(orphanages);
   },
@@ -16,7 +18,9 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanage = await orphanagesRepository.findOneOrFail(id);
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ['images']
+    });
 
     return response.json(orphanage);
   },
@@ -33,6 +37,11 @@ export default {
     } = request.body;
   
     const orphanagesRepository = getRepository(Orphanage);
+
+    const requestImages = request.files as Express.Multer.File[];
+    const images = requestImages.map(image => {
+      return { path: image.filename }
+    });
   
     const orphanage = orphanagesRepository.create({
       name,
@@ -41,7 +50,8 @@ export default {
       about,
       instructions,
       opening_hours,
-      open_on_weekends
+      open_on_weekends,
+      images
     });
   
     await orphanagesRepository.save(orphanage);
